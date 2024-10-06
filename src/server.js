33064225fd8518aeebf30a355a75a848ea163f8d
hungdungn47 <hungdungn47@gmail.com>
2024-10-06 09:41:50 +0700

    Cors options

--- src/server.js
+++ src/server.js
@@ -9,9 +9,13 @@ import { connectDb } from '~/config/mongodb'
 import { env } from '~/config/environment'
 import { APIs_V1 } from '~/routes/v1'
 import { errorHandlingMiddleware } from '~/middlewares/errorHandlingMiddleware'
+import cors from 'cors'
+import { corsOptions } from './config/cors'
 
 const startServer = () => {
   const app = express()
+
+  app.use(cors(corsOptions))
   // Enable req.body json data
   app.use(express.json())
 
