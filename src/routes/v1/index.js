af6a3ab82b310628c4f6e5b54a0586820b9bd937
hungdungn47 <hungdungn47@gmail.com>
2024-10-09 07:06:51 +0700

    Add new column and card API

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -6,6 +6,8 @@
 import express from 'express'
 import { StatusCodes } from 'http-status-codes'
 import { boardRoute } from './boardRoute'
+import { columnRoute } from './columnRoute'
+import { cardRoute } from './cardRoute'
 
 const Router = express.Router()
 
@@ -16,5 +18,7 @@ Router.get('/status', (req, res) => {
 })
 
 Router.use('/boards', boardRoute)
+Router.use('/columns', columnRoute)
+Router.use('/cards', cardRoute)
 
 export const APIs_V1 = Router
