4070470679ce4c6c545a0ac75d5addda56902949
hungdungn47 <hungdungn47@gmail.com>
2024-11-13 18:52:07 +0700

    Fix whitelist domain

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -5,7 +5,7 @@
  */
 export const WHITELIST_DOMAINS = [
   'http://localhost:5173',
-  'https://trello-web-sable-kappa.vercel.app/'
+  'https://trello-web-sable-kappa.vercel.app'
 ]
 
 export const BOARD_TYPES = {
